9494b193e4f76bb97a54611ddd1f5d8af520bb3a
Sharkuu <sharku@op.pl>
2016-11-30 16:47:00 +0100

    srednie wszystkich panstw po wszystkich latach dla 1 zrodla, wykres wszystkich wartosci dla wszyrstkich lat(1 zrodlo) NIECZYTELNE JAK CH...

--- projekt.R
+++ projekt.R
@@ -10,9 +10,12 @@ data.ch4$V46 <- NULL
 names(data.ch4) <- sapply(read.csv2("v4.2_CH4_tot_1970_2008.csv")[9,3:45], as.character)
 data.ch4[is.na(data.ch4)] <-0.0
 
-na.omit(data.ch4)
 
 plot(names(data.ch4)[5:43],data.ch4[1,5:43],type = "l")
 
-t<- (data.ch4[seq(from = 1, to = 879, by=23),5:43])
-t <- colMeans(t)
+public.electricity.heat<-subset(data.ch4, data.ch4[,4] == "Public electricity and heat production")
+
+public.electricity.heat$mean <- rowMeans(public.electricity.heat[,5:43])
+
+
+plot(public.electricity.heat$Name,public.electricity.heat$mean)
