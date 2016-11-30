d5e1302f9ac8a0150e53a50edcda6ad6a994d7d7
Sharkuu <sharku@op.pl>
2016-11-30 16:52:29 +0100

    skala zmieniona >>>>nic nei widac, trzeba wybrac dane

--- projekt.R
+++ projekt.R
@@ -8,8 +8,8 @@ data.ch4$V1 <-NULL
 data.ch4$V2 <-NULL
 data.ch4$V46 <- NULL
 names(data.ch4) <- sapply(read.csv2("v4.2_CH4_tot_1970_2008.csv")[9,3:45], as.character)
-data.ch4[is.na(data.ch4)] <-0.0
-
+#data.ch4[is.na(data.ch4)] <-0.0
+na.omit(data.ch4)
 
 plot(names(data.ch4)[5:43],data.ch4[1,5:43],type = "l")
 
@@ -18,4 +18,19 @@ public.electricity.heat<-subset(data.ch4, data.ch4[,4] == "Public electricity an
 public.electricity.heat$mean <- rowMeans(public.electricity.heat[,5:43])
 
 
-plot(public.electricity.heat$Name,public.electricity.heat$mean)
+dotplot(mean~ Name, data = public.electricity.heat)
+
+public.electricity.heat[which.min(public.electricity.heat$mean),]
+
+
+for(i in 1:nrow(public.electricity.heat)){
+  if(i==1){
+  plot(names(public.electricity.heat)[5:43],public.electricity.heat[1,5:43],type = "l", col = 1, ylim=c(0,13))
+    
+  }else{
+  
+    
+    lines(names(public.electricity.heat)[5:43],public.electricity.heat[i,5:43],type = "l", col = i)
+  }
+  
+}
