8b1f03f6281ab9c4cc6279b6e74a83d0d73ce849
Sharkuu <sharku@op.pl>
2016-12-03 16:32:39 +0100

    powoli do przodu

--- projekt.R
+++ projekt.R
@@ -50,28 +50,24 @@ for(i in country_names){
   x<-data.ch4[data.ch4$Name == i,]
   k<-x[,5:15]
   s<-rowMeans(k,na.rm = TRUE)
-  m<-mean(s,na.rm = TRUE)
-  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,3] <-m
+  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,3] <-mean(s,na.rm = TRUE)
   
   k<-x[,12:25]
   s<-rowMeans(k,na.rm = TRUE)
-  m<-mean(s,na.rm = TRUE)
-  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,4] <-m
+  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,4] <-mean(s,na.rm = TRUE)
   
   k<-x[,22:35]
   s<-rowMeans(k,na.rm = TRUE)
-  m<-mean(s,na.rm = TRUE)
-  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,5] <-m
+  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,5] <-mean(s,na.rm = TRUE)
   
   k<-x[,32:43]
   s<-rowMeans(k,na.rm = TRUE)
-  m<-mean(s,na.rm = TRUE)
-  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,6] <-m
+  srednie.wszystkie.panstwa.wszystkie.zrodla[srednie.wszystkie.panstwa.wszystkie.zrodla$Name == i,6] <-mean(s,na.rm = TRUE)
   
   
   
 }
-rm(k,s,m,x)
+
 #srednia calosciowa i segregowanie
 srednie.wszystkie.panstwa.wszystkie.zrodla$summary.mean <-rowMeans(srednie.wszystkie.panstwa.wszystkie.zrodla[,3:6])
 srednie.wszystkie.panstwa.wszystkie.zrodla<- srednie.wszystkie.panstwa.wszystkie.zrodla[with(srednie.wszystkie.panstwa.wszystkie.zrodla, order(-summary.mean)), ]
@@ -80,3 +76,54 @@ barchart(Name ~ date1970.1980, data = srednie.wszystkie.panstwa.wszystkie.zrodla
 barchart(Name ~ date1981.1990, data = srednie.wszystkie.panstwa.wszystkie.zrodla[1:3,c(2,4)])
 barchart(Name ~ date1991.2000, data = srednie.wszystkie.panstwa.wszystkie.zrodla[1:3,c(2,5)])
 barchart(Name ~ date2001.2008, data = srednie.wszystkie.panstwa.wszystkie.zrodla[1:3,c(2,6)])
+
+
+#######dla kazdego zrodla, wszystkie panstwa, dekadowo
+source_names <- unique(data.ch4$IPCC_description)
+
+srednie.wszystkie.zrodla = data.frame(zrodlo = unique(data.ch4$IPCC_description),
+                                                        date1970.1980 = NA,
+                                                        date1981.1990 = NA,
+                                                        date1991.2000 =NA,
+                                                        date2001.2008 = NA,
+                                                        summary.mean = NA
+)
+
+for (i in source_names) {
+  x<-data.ch4[data.ch4$IPCC_description == i,]
+  k<-x[,5:15]
+  s<-rowMeans(k,na.rm = TRUE)
+  srednie.wszystkie.zrodla[srednie.wszystkie.zrodla$zrodlo == i,2] <-mean(s,na.rm = TRUE)
+  
+  k<-x[,12:25]
+  s<-rowMeans(k,na.rm = TRUE)
+  srednie.wszystkie.zrodla[srednie.wszystkie.zrodla$zrodlo == i,3] <-mean(s,na.rm = TRUE) 
+  
+  k<-x[,22:35]
+  s<-rowMeans(k,na.rm = TRUE)
+  srednie.wszystkie.zrodla[srednie.wszystkie.zrodla$zrodlo == i,4] <-mean(s,na.rm = TRUE)
+  
+  k<-x[,32:43]
+  s<-rowMeans(k,na.rm = TRUE)
+  srednie.wszystkie.zrodla[srednie.wszystkie.zrodla$zrodlo == i,5] <-mean(s,na.rm = TRUE) 
+  
+}
+rm(k,s,x)
+
+srednie.wszystkie.zrodla$summary.mean <-rowMeans(srednie.wszystkie.zrodla[,2:5])
+srednie.wszystkie.zrodla<- srednie.wszystkie.zrodla[with(srednie.wszystkie.zrodla, order(-summary.mean)), ]
+
+####wykresy zrodel wybranych(2 top 2 middle, 2 end) rocznie (nie moga byc na jednym bo skala jest pojebana)
+j<-1
+for(i in srednie.wszystkie.zrodla[c(1,2,11,12,20,21),1]){
+  tmp <- subset(data.ch4, data.ch4[,4] == as.name(i))
+  tmp <- colMeans(tmp[,5:43],na.rm = TRUE)
+ if(j==1){
+    plot(names(tmp),tmp,type = "l", col = j, xlab = "rok", ylab = as.name(i))
+   j<-j+1
+}else{
+  print(tmp)
+ plot(names(tmp),tmp,type = "l", col = j, xlab = "rok", ylab = as.name(i))
+  j<- j+1
+}}
+
